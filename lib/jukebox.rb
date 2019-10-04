da0c99d42884cb4087247e1cf32dbc2bb0f74646
Hamidou Diallo <dio.hamidou@gmail.com>
2019-10-04 05:36:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,11 +33,7 @@ def play(songs)
   user_response = gets.strip
   songs.each do |music|
     if user_response.to_s == music 
-      puts ""
-      p music
-      puts ""
-      p user_response
-      puts ""
+      puts music
     elsif user_response.to_i == (songs.index(music)+1)
       puts "Playing #{music}"
     else
